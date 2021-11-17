199f7e78ea1f807ff381c412a46538d9a329e632
Mark <mark@example.dev>
2021-11-16 20:17:21 -0800

    useAxios hook part 1

--- src/hooks.js
+++ src/hooks.js
@@ -1,3 +1,5 @@
+import Axios from 'axios';
+import uuid from 'uuid';
 import {useState} from 'react';
 
 function useFlip(initialState=true){
@@ -8,4 +10,15 @@ function useFlip(initialState=true){
   return [state, flipState];
 }
 
-export { useFlip };
+// Returns a piece of state that is an array, and a function that makes a request to given URL and adds data from
+// the response to the array state.
+function useAxios(url){
+  const [state, setState] = useState([]);
+  const addToState = async () => {
+    const response = await Axios.get(url);
+    setState(state => [...state, {...response.data, id: uuid() }]);
+  }
+  return [state, addToState];
+}
+
+export { useFlip, useAxios };
